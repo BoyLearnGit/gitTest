e30d20cb660d2b0dc1cedd3872e09c4a67c0afe3
liyanlu <liyanlu@youedata.com>
2017-09-01 14:13:47 +0800

    Signed-off-by: liyanlu <liyanlu@youedata.com>

--- gitTest/src/main/java/com/youedata/gitTest/App.java
+++ gitTest/src/main/java/com/youedata/gitTest/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Wld!" );
         System.out.println("111");
     }
 }
